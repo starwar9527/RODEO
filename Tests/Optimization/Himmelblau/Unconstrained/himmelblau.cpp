fc28fe9549a5d58862f07f95a7cedc1db9ed05f5
KaiChengDM <101462680+KaiChengDM@users.noreply.github.com>
2022-06-27 13:48:19 +0200

    Add files via upload

--- Tests/Optimization/Himmelblau/Unconstrained/himmelblau.cpp
+++ Tests/Optimization/Himmelblau/Unconstrained/himmelblau.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 
+
 double Himmelblau(double *x){
 	
 	return pow( (x[0]*x[0]+x[1]-11.0), 2.0 ) + pow( (x[0]+x[1]*x[1]-7.0), 2.0 );
@@ -8,16 +9,27 @@ double Himmelblau(double *x){
 
 int main(void){
 
-double x[2];
-FILE *inp = fopen("dv.dat","r");
-fscanf(inp,"%lf",&x[0]);
-fscanf(inp,"%lf",&x[1]);
+//double x[2];
 
+//FILE *inp = fopen("dv.dat","r");
+//fscanf(inp,"%lf",&x[0]);
+//fscanf(inp,"%lf",&x[1]);
+//fclose(inp);
+
+
+double x[2];
+
+FILE *inp = fopen("dv.dat","r");
+
+for (unsigned int k = 0; k < sizeof(x); k++ ){
+	fscanf(inp,"%lf",&x[k]);
+}
 fclose(inp);
+
 
 double result = Himmelblau(x);
 FILE *outp = fopen("objFunVal.dat","w");
-fprintf(outp,"J = %15.10f\n",result);
+fprintf(outp,"himmelblau_function = %15.10f\n",result);
 fclose(outp);
 
 
