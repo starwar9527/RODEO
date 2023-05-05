255590d67c9712e697fdc7bb6353147101e0a916
Xinghua Song <xinghua.song@essteyr.com>
2023-05-05 09:58:19 +0200

    delete build directories

--- build/src/correlation_functions.cpp.d
+++
@@ -1,7 +0,0 @@
-build/./src/correlation_functions.cpp.o: src/correlation_functions.cpp \
- src/INCLUDE/correlation_functions.hpp \
- src/INCLUDE/correlation_functions.hpp
-
-src/INCLUDE/correlation_functions.hpp:
-
-src/INCLUDE/correlation_functions.hpp:
--- build/src/output.cpp.d
+++
@@ -1,8 +0,0 @@
-build/./src/output.cpp.o: src/output.cpp src/INCLUDE/output.hpp \
- src/INCLUDE/matrix_vector_operations.hpp src/INCLUDE/bounds.hpp
-
-src/INCLUDE/output.hpp:
-
-src/INCLUDE/matrix_vector_operations.hpp:
-
-src/INCLUDE/bounds.hpp:
